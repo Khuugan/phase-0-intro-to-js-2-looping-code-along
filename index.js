9ec82ef01b811143f86772de94d84ff62227a748
Narankhuu Ganbaatar <narankhuuu@gmail.com>
2023-09-23 15:36:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  let names = ['Khuu', 'Eric', 'Greg'];
+  let eventName = 'surprise';
+  let result = writeCards(names, eventName);
+  console.log(result);
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
